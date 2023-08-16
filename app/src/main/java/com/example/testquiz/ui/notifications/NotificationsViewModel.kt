b118d27cd743497f881e9202022b189d1cac0939
Jess-Windows <jessicasantanacampos@gmail.com>
2023-08-16 19:00:10 -0300

    Rank Page Updated

--- app/src/main/java/com/example/testquiz/ui/notifications/NotificationsViewModel.kt
+++ app/src/main/java/com/example/testquiz/ui/notifications/NotificationsViewModel.kt
@@ -7,7 +7,8 @@ import androidx.lifecycle.ViewModel
 class NotificationsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "Melhores Rank"
+        value = "Melhores Rank"
     }
     val text: LiveData<String> = _text
 }
